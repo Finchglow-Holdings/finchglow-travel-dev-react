6d1ba8726a75f6324f157594347e9d97d54468b0
Casmir Okorie <casmir456@gmail.com>
2024-07-23 21:01:58 +0100

    updating site

--- src/components/contents/sections/achievement.jsx
+++ src/components/contents/sections/achievement.jsx
@@ -33,24 +33,15 @@ const AchievementsSection = () => {
       <div className="static w-full flex flex-col justify-center items-center 2xl:w-[1280px] px-[15px] py-[50px] at500:pl-[80px] at500:pr-[40px] my-0 mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center w-full gap-[30px]">
           <div className="relative flex flex-col items-start gap-[17px] w-full">
-            <span
-              
-              className="txt2 text-[#0260EE] capitalize mb-[10px]"
-            >
+            <span className="txt2 text-[#0260EE] capitalize mb-[10px]">
               Key Achievements
             </span>
-            <h3
-              
-              className="text-[#FFFFFF] !text-[30px] !leading-[1.1] lg:w-[452px] mb-[10px]"
-            >
+            <h3 className="text-[#FFFFFF] !text-[30px] !leading-[1.1] lg:w-[452px] mb-[10px]">
               We’ve mastered connecting our clients with the rest of the world
               and perfected providing exceptional service and travel
               experiences.
             </h3>
-            <p
-            
-              className="leading-[24px] text-[16px] text-[#E6EFFD] lg:w-[452px] mb-[20px]"
-            >
+            <p className="leading-[24px] text-[16px] text-[#E6EFFD] lg:w-[452px] mb-[20px]">
               Since we launched in June 2006, we have expanded from one branch
               to nine branches across Nigeria, won over 20 industry awards, and
               serviced over 20,000 clients. We are dedicated to offering the
@@ -73,32 +64,26 @@ const AchievementsSection = () => {
                   alt="dollor sign"
                 />
               </span>
-              <div  className="counter" ref={nairaRef}>
+              <div className="counter" ref={nairaRef}>
                 {nairaInView && (
-                  <CountUp end={20} suffix="+ million" duration={3} />
+                  <CountUp end={30} suffix="+ Billion" duration={3} />
                 )}
               </div>
-              <span className="txt3 capitalize">
-                Travel Partners worldwide
-              </span>
+              <span className="txt3 capitalize">Naira Annual Turnover</span>
             </div>
 
             <div className="flex flex-col justify-center gap-[8px] items-start w-full">
-              <span >
+              <span>
                 <img
                   src={icon2}
                   className="w-[46px] h-auto object-cover"
                   alt="dollor sign"
                 />
               </span>
-              <div  className="counter" ref={tripsRef}>
-                {tripsInView && (
-                  <CountUp end={100} suffix="K+" duration={3} />
-                )}
+              <div className="counter" ref={tripsRef}>
+                {tripsInView && <CountUp end={100} suffix="K+" duration={3} />}
               </div>
-              <span  className="txt3 capitalize">
-                Trips booked
-              </span>
+              <span className="txt3 capitalize">Trips booked</span>
             </div>
 
             <div className="flex flex-col justify-center gap-[8px] items-start w-full">
@@ -109,18 +94,16 @@ const AchievementsSection = () => {
                   alt="dollor sign"
                 />
               </span>
-              <div  className="counter" ref={consultantsRef}>
+              <div className="counter" ref={consultantsRef}>
                 {consultantsInView && (
                   <CountUp end={200} suffix="+" duration={3} />
                 )}
               </div>
-              <span className="txt3 capitalize">
-                Travel Consultants
-              </span>
+              <span className="txt3 capitalize">Travel Consultants</span>
             </div>
 
             <div className="flex flex-col justify-center gap-[8px] items-start w-full">
-              <span >
+              <span>
                 <img
                   src={icon4}
                   className="w-[46px] h-auto object-cover"
@@ -128,13 +111,9 @@ const AchievementsSection = () => {
                 />
               </span>
               <div className="counter" ref={clientsRef}>
-                {clientsInView && (
-                  <CountUp end={20} suffix="k+" duration={3} />
-                )}
+                {clientsInView && <CountUp end={20} suffix="k+" duration={3} />}
               </div>
-              <span className="txt3 capitalize">
-                Happy clients
-              </span>
+              <span className="txt3 capitalize">Happy clients</span>
             </div>
           </div>
         </div>
