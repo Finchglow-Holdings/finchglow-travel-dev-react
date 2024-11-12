d0a825e0ac2952efc25960ae195de7530ea51468
Casmir Okorie <casmir456@gmail.com>
2024-11-12 14:03:49 +0100

    adding server-proxy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/partner-plus-connect": {
+        target: "https://partnerplus.finchglowtravels.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/partner-plus-connect/, '')
+      },"/_next": {
+        target: "https://partnerplus.finchglowtravels.com",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/partner-plus-connect/, '')
+      },"/images": {
+        target: "https://partnerplus.finchglowtravels.com",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/partner-plus-connect/, '')
+      }
+    }
+  }
+});
